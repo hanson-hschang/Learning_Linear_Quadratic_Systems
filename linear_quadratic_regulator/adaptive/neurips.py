03f68782fda134a9e648718586ce4feb8c8dd59c
Heng-Sheng Chang <hschang@wirelessprv-10-192-250-73.near.illinois.edu>
2024-05-20 11:00:37 -0500

    Add for loop

--- linear_quadratic_regulator/adaptive/neurips.py
+++ linear_quadratic_regulator/adaptive/neurips.py
@@ -37,8 +37,8 @@ from mflq import MFLQStrategy
 
 # logging.basicConfig(level=logging.CRITICAL)
 logging.basicConfig(
-    level=logging.DEBUG, 
-    filename="regret_comparison.log", 
+    level=logging.INFO, 
+    filename="neurips.log", 
     filemode="w",
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
@@ -133,7 +133,6 @@ print(K_inf)
 # J_inf = utils.LQR_cost(A_star, B_star, K_inf, Q, R, sigma_w)
 
 
-prime_horizon = 5000
 def optimal_ctor():
     return OptimalStrategy(Q=Q, R=R, A_star=A_star, B_star=B_star, sigma_w=sigma_w)
 
@@ -252,106 +251,118 @@ ctor = {
 # strategies = ['LSPI']
 # list_of_results = []
 # start_time = time.time()
-method = 'LSPI'
-regrets = []
-errors = []
-costs = []
-seeds = []
-Kts = []
-times = []
-bad_invocations = 0
-start_time = time.time()
-for i in range(trials_per_method):
-    logging.info(method + " with trial No. " + str(i+1))
-    seed = np.random.randint(0xFFFFFFFF)
-    try:
-        reg, err, cost, Kt_iterations, time_iterations = run_one_trial(ctor[method], seed, prime_fixed=True)
-        regrets.append(reg)
-        errors.append(err)
-        costs.append(cost)
-        Kts.append(Kt_iterations)
-        times.append(time_iterations)
-        seeds.append(seed)
-    except Exception as e:
-        traceback.print_exc()
-        bad_invocations += 1
-# result = (np.array(regrets), np.array(errors), np.array(costs), np.array(Kts), np.array(times), np.array(seeds), bad_invocations)
-
-print("finished execution in {} seconds with {} many time trials".format(time.time() - start_time, trials_per_method))
 
 
 def calculate_K_error(Kt_iterations, K_inf):
     return np.array([np.linalg.norm(Kt - K_inf, ord='fro')/np.linalg.norm(K_inf, ord='fro') for Kt in Kt_iterations])
 
+method = 'LSPI'
+prime_horizon = 7500
 
-plt.rcParams['text.usetex'] = True
-fontsize = 18
-gain_K_fig = plt.figure()
-gain_K_ax = gain_K_fig.subplots(1, 1)
-cost_fig = plt.figure()
-cost_ax = cost_fig.subplots(1,1)
+for prime_horizon in [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500,]:
+    regrets = []
+    errors = []
+    costs = []
+    seeds = []
+    Kts = []
+    times = []
+    bad_invocations = 0
+    start_time = time.time()
+    for i in range(trials_per_method):
+        logging.info(method + " with trial No. " + str(i+1))
+        seed = np.random.randint(0xFFFFFFFF)
+        try:
+            reg, err, cost, Kt_iterations, time_iterations = run_one_trial(ctor[method], seed, prime_fixed=True)
+            regrets.append(reg)
+            errors.append(err)
+            costs.append(cost)
+            Kts.append(Kt_iterations)
+            times.append(time_iterations)
+            seeds.append(seed)
+        except Exception as e:
+            traceback.print_exc()
+            bad_invocations += 1
+    # result = (np.array(regrets), np.array(errors), np.array(costs), np.array(Kts), np.array(times), np.array(seeds), bad_invocations)
 
-neuirps_data_folder_directory = "neurips_data/"
-if not os.path.isdir(neuirps_data_folder_directory):
-    os.mkdir(neuirps_data_folder_directory)
-
-
-for ind, (cost_iterations, Kt_iterations, time_iterations) in enumerate(zip(costs, Kts, times)):
-
-    # print(ind)
-    # print(cost_iterations.shape)
-    # print(Kt_iterations.shape)
-    # print(time_iterations.shape)
-
-    K_error_iterations = calculate_K_error(Kt_iterations, K_inf)
-    cost_iterations = np.insert(cost_iterations, 0, np.inf)
-    time_iterations = time_iterations - time_iterations[0]
-
-    min_K_error = K_error_iterations[0]
-    min_cost = cost_iterations[0]
-    for i in range(1, len(time_iterations)):
-        if K_error_iterations[i] < min_K_error:
-            min_K_error = K_error_iterations[i]
-        else:
-            K_error_iterations[i] = min_K_error
-        if cost_iterations[i] < min_cost:
-            min_cost = cost_iterations[i]
-        else:
-            cost_iterations[i] = min_cost
-
-    gain_K_ax.plot(
-        K_error_iterations[1:],
-        time_iterations[1:]
-    )
-    cost_ax.plot(
-        cost_iterations[1:],
-        time_iterations[1:]
+    print(
+        "finished execution of {} prime horizon in {} seconds with {} many time trials".format(
+            prime_horizon, time.time() - start_time, trials_per_method
+        )
     )
 
-    print("Saving benchmark file with ind = " + str(ind))
-    # benchmark algo list filenames
-    data_file_name = neuirps_data_folder_directory + "Benjamin_ind_" + str(ind) + ".pk"
-    
-    data_file = open(data_file_name, "wb")
+    neuirps_data_folder_directory = "neurips_data/"
+    if not os.path.isdir(neuirps_data_folder_directory):
+        os.mkdir(neuirps_data_folder_directory)
 
-    pickle.dump(
-        dict(
-            benchmark_time=time_iterations,
-            benchmark_normalized_K_error=K_error_iterations,
-            benchmark_normalized_cost=cost_iterations
-        ), 
-        data_file
-    )
-
-    data_file.close()
+    neuirps_data_folder_directory += "prime_horizon_" + str(prime_horizon) + "/"
+    if not os.path.isdir(neuirps_data_folder_directory):
+        os.mkdir(neuirps_data_folder_directory)
 
 
-gain_K_ax.set_xscale('log')
-gain_K_ax.set_xlabel("normalized error", fontsize=fontsize)
-gain_K_ax.set_ylabel("time [sec]", fontsize=fontsize)
-cost_ax.set_xscale('log')
-cost_ax.set_xlabel(r'$(J-J^*)/J^*$', fontsize=fontsize)
-cost_ax.set_ylabel("time [sec]", fontsize=fontsize)
+    for ind, (cost_iterations, Kt_iterations, time_iterations) in enumerate(zip(costs, Kts, times)):
+
+        # print(ind)
+        # print(cost_iterations.shape)
+        # print(Kt_iterations.shape)
+        # print(time_iterations.shape)
+
+        K_error_iterations = calculate_K_error(Kt_iterations, K_inf)
+        cost_iterations = np.insert(cost_iterations, 0, np.inf)
+        time_iterations = time_iterations - time_iterations[0]
+
+        min_K_error = K_error_iterations[0]
+        min_cost = cost_iterations[0]
+        for i in range(1, len(time_iterations)):
+            if K_error_iterations[i] < min_K_error:
+                min_K_error = K_error_iterations[i]
+            else:
+                K_error_iterations[i] = min_K_error
+            if cost_iterations[i] < min_cost:
+                min_cost = cost_iterations[i]
+            else:
+                cost_iterations[i] = min_cost
+
+        # gain_K_ax.plot(
+        #     K_error_iterations[1:],
+        #     time_iterations[1:]
+        # )
+        # cost_ax.plot(
+        #     cost_iterations[1:],
+        #     time_iterations[1:]
+        # )
+
+        print("Saving benchmark file with ind = " + str(ind))
+        # benchmark algo list filenames
+        data_file_name = neuirps_data_folder_directory + "Benjamin_ind_" + str(ind) + ".pk"
+        
+        data_file = open(data_file_name, "wb")
+
+        pickle.dump(
+            dict(
+                benchmark_time=time_iterations,
+                benchmark_normalized_K_error=K_error_iterations,
+                benchmark_normalized_cost=cost_iterations
+            ), 
+            data_file
+        )
+
+        data_file.close()
 
 
-plt.show()
+# plt.rcParams['text.usetex'] = True
+# fontsize = 18
+# gain_K_fig = plt.figure()
+# gain_K_ax = gain_K_fig.subplots(1, 1)
+# cost_fig = plt.figure()
+# cost_ax = cost_fig.subplots(1,1)
+
+
+# gain_K_ax.set_xscale('log')
+# gain_K_ax.set_xlabel("normalized error", fontsize=fontsize)
+# gain_K_ax.set_ylabel("time [sec]", fontsize=fontsize)
+# cost_ax.set_xscale('log')
+# cost_ax.set_xlabel(r'$(J-J^*)/J^*$', fontsize=fontsize)
+# cost_ax.set_ylabel("time [sec]", fontsize=fontsize)
+
+
+# plt.show()
