f496ff67496a49be6542eba547555dc1223138d4
Simulation <hanson.hschang@gmail.com>
2024-05-20 20:10:51 -0400

    Update prime_horizon_list

--- linear_quadratic_regulator/adaptive/neurips.py
+++ linear_quadratic_regulator/adaptive/neurips.py
@@ -259,7 +259,7 @@ def calculate_K_error(Kt_iterations, K_inf):
 method = 'LSPI'
 prime_horizon = 7500
 
-for prime_horizon in [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500,]:
+for prime_horizon in [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]:
     regrets = []
     errors = []
     costs = []
